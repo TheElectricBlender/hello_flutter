f34ad2b59114526fb1f0801823e675f5c62bc466
TheElectricBlender <yodudee97@gmail.com>
2020-02-20 12:12:49 +0800

    Minor UI changes: 1. appBar from blue to red 2. floatingActionButton from blue to red 3. changed font to custom IndieFlower

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,24 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text("My first flutter"),
       centerTitle: true,
+      backgroundColor: Colors.red[600],
     ),
     body: Center(
-      child: Text("hello!"),
+      child: Text(
+        "hello NERDS!",
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+          fontFamily: "IndieFlower",
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
+      onPressed: () {},
       child: Text("click"),
+      backgroundColor: Colors.red[600],
     ),
   ),
 ));
