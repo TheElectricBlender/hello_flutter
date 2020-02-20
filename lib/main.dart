697118ea16c698f79e4599ad10dff0d000ff8f8d
TheElectricBlender <yodudee97@gmail.com>
2020-02-20 11:55:10 +0800

    added header bar and click button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text("Hello Flutter!"),
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("My first flutter"),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text("hello!"),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text("click"),
+    ),
+  ),
 ));
 
